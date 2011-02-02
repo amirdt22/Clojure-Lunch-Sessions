14cb77e89bb10ab5877be451359a3f0b135ded46
Audrey Troutt <audrey.troutt@gmail.com>
2011-02-02 13:19:42 -0500

    Two more down after clojure lunch today. Hard to remember everything from last time.

--- src/koans/sequence_comprehensions.clj
+++ src/koans/sequence_comprehensions.clj
@@ -1,6 +1,6 @@
 (meditations
   "Sequence comprehensions can bind each element in turn to a symbol"
-  (= __
+  (= '(0 1 2 3 4 5) 
      (for [index (range 6)]
        index))
 
@@ -8,25 +8,26 @@
   (= '(0 1 4 9 16 25)
      (map (fn [index] (* index index))
           (range 6))
-     (for [index (range 6)]
-       __))
+     (for [index (range 6)] 
+       (* index index)))
 
   "And also filtering"
   (= '(1 3 5 7 9)
      (filter odd? (range 10))
-     (for [index __ :when (odd? index)]
+     (for [index (range 10) :when (odd? index)]
        index))
 
   "And they trivially allow combinations of the two transformations"
   (= '(1 9 25 49 81)
      (map (fn [index] (* index index))
           (filter odd? (range 10)))
-     (for [index (range 10) :when __]
-       __))
+     (for [index (range 10) :when (odd? index)]
+       (* index index)))
 
   "More complex transformations can be formed with multiple binding forms"
   (= [[:top :left] [:top :middle] [:top :right]
       [:middle :left] [:middle :middle] [:middle :right]
       [:bottom :left] [:bottom :middle] [:bottom :right]]
      (for [row [:top :middle :bottom] column [:left :middle :right]]
-          __)))
+          [row column]))
+)
